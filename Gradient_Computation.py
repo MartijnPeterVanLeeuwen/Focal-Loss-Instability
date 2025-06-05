cf99fdaec8c0fb11c67a60e7c8d5437df7e6861e
MartijnPeterVanLeeuwen <m.p.vanleeuwen@tilburguniversity.edu>
2025-06-05 13:18:18 +0200

    Create Gradient_Computation.py

---
+++ Gradient_Computation.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision
+import numpy as np
+import sigmoid_focal_loss_modified
+Sigmoid=torch.nn.Sigmoid()
+
+alpha=0.5
+gamma=2
+steps=10000
+outputs=np.linspace(-38,38,steps)
+
+labels=torch.tensor(1.,dtype=torch.float64) # GT label, 0.0 for background gradient
+y=Sigmoid(torch.tensor(outputs,dtype=torch.float64))
+Stable_loss=False
+epsilon=1e-3
+for i in range(steps):
+    y_t=torch.tensor(outputs[i]*1.0,requires_grad=True)
+    if Stable_loss==False:
+        loss=torchvision.ops.sigmoid_focal_loss(y_t,labels,
+             alpha=alpha,gamma=gamma,reduction='mean)
+    else:
+        loss=sigmoid_focal_loss_modified(y_t,labels,
+             alpha=alpha,gamma=gamma,reduction='mean,
+             epsilon=epsilon)
+         
+    loss.backward()
+    Gradient=y_t.grad.item()
+    print(Gradient)
