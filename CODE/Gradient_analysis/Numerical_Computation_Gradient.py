74b06cc54b3c509b8fa65bcf2a7012e04901203c
Martijn van Leeuwen <M.P.vanLeeuwen@tilburguniversity.edu>
2025-10-29 11:28:45 +0100

    tweak gradient computation code

--- CODE/Gradient_analysis/Numerical_Computation_Gradient.py
+++ CODE/Gradient_analysis/Numerical_Computation_Gradient.py
@@ -1,12 +1,18 @@
 import torch
 import torchvision
 import numpy as np
-import sigmoid_focal_loss_modified
+import os 
+import sys
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+from Stabilized_Focal_loss import sigmoid_focal_loss_modified
+
 Sigmoid=torch.nn.Sigmoid()
 
 alpha=0.5
-gamma=2
-steps=10000
+gamma=0.5
+steps=100
 outputs=np.linspace(-38,38,steps)
 
 labels=torch.tensor(1.,dtype=torch.float64) # GT label, 0.0 for background gradient
@@ -17,12 +23,9 @@ epsilon= 1e-3
 for i in range(steps):
     y_t=torch.tensor(outputs[i]*1.0,requires_grad=True)
     if Stable_loss==False:
-        loss=torchvision.ops.sigmoid_focal_loss(y_t,labels,
-             alpha=alpha,gamma=gamma,reduction='mean)
+        loss=torchvision.ops.sigmoid_focal_loss(y_t,labels,alpha=alpha,gamma=gamma,reduction='mean')
     else:
-        loss=sigmoid_focal_loss_modified(y_t,labels,
-             alpha=alpha,gamma=gamma,reduction='mean,
-             epsilon=epsilon)
+        loss=sigmoid_focal_loss_modified(y_t,labels,alpha=alpha,gamma=gamma,reduction='mean',epsilon=epsilon)
          
     loss.backward()
     Gradient=y_t.grad.item()
